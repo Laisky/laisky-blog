8f9d50c6f47e406e6cbd5a8a04807ca22544859b
Laisky <ppcelery@gmail.com>
2015-01-23 21:52:18 +0800

    #1: framwork

---
+++ README.md
@@ -0,0 +1,24 @@
+Gargantua
+---
+Laisky's new blog by hand
+
+## Description
+
+A simple blog by Python3
+
+## Infrastructure
+
+- Python3
+    - tornado
+    - jinja2
+    - Motor
+
+- MongoDB
+    - TokuMX
+
+- Nginx
+
+- HTML5
+    - CSS3 / LESS
+    - JavaScript / jQuery
+    - Bootstrap2
---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools import find_packages
+from pip.req import parse_requirements
+from pip.download import PipSession
+
+
+requires = [str(i.req) for i in parse_requirements('requirements.txt',
+                                                   session=PipSession())
+            if i.req is not None]
+
+setup(name='gargantua',
+      version='0.1-dev',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      include_package_data=True,
+      install_requires=requires)
