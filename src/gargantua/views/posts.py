45205a9dbb48b7097285b7629df7792403f1be8a
Laisky <ppcelery@gmail.com>
2015-03-31 23:26:14 +0800

    fix typo

--- src/gargantua/views/posts.py
+++ src/gargantua/views/posts.py
@@ -41,7 +41,7 @@ class PostsHandler(BaseHandler):
         since_id = (yield
                     self.db.posts.find_one({'post_name': since_name}))['_id']
         cursor = self.db.posts.find({'_id': {'$lt': since_id},
-                                     'post_password': {'$ne': ''}})
+                                     'post_password': {'$eq': ''}})
         cursor.sort([('_id', pymongo.DESCENDING)]).limit(n)
         posts = []
         for docu in (yield cursor.to_list(length=n)):
