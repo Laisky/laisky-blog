ddf1fdcd7091000e584362f6e3e6c5c2414200ec
Laisky <ppcelery@gmail.com>
2015-05-04 23:20:26 +0800

    #25: done

---
+++ src/gargantua/static/js/pages/publish.js
@@ -0,0 +1,39 @@
+$(function() {
+    bindPublishForm();
+
+
+    function bindPublishForm() {
+        $(".publish-body").on("submit", "#publishForm", function() {
+            var $postTitle = $("#postTitleInput");
+            var $postName = $("#postNameInput");
+            var $postContent = $("#postContentInput");
+
+            var url = "/publish/";
+            var args = {
+                postTitle: $postTitle.val(),
+                postName: $postName.val(),
+                postContent: $postContent.val()
+            };
+
+            $.postJSON(url, args, function(resp) {
+                var $hint = $(".hint span");
+
+                $hint.html(resp.msg);
+                if(resp.status == 0) {
+                    $hint.removeClass('label-info');
+                    $hint.removeClass('label-danger');
+                    $hint.addClass('label-success');
+                    $hint.html("发布成功，正在跳转...");
+                    setTimeout(function() {
+                        location.href = "/archives/?page=1";
+                    }, 1000);
+                }else {
+                    $hint.removeClass('label-info');
+                    $hint.addClass('label-danger');
+                }
+            });
+
+            return false;
+        })
+    }
+})
