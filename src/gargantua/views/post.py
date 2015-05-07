66216451dcd33bd48c70a777ac387ea82ffadcf6
Laisky <ppcelery@gmail.com>
2015-05-07 22:28:17 +0800

    #29: add reveal.js

--- src/gargantua/views/post.py
+++ src/gargantua/views/post.py
@@ -15,18 +15,22 @@ log = logging.getLogger(LOG_NAME)
 
 class PostPage(BaseHandler):
 
-    # @tornado.gen.coroutine
-    # @debug_wrapper
-    # def get(self, name):
-    #     log.info('GET PostPage for name {}'.format(name))
-
-    #     name = urllib.parse.quote(name).lower()
-    #     post = yield self.db.posts.find_one({'post_name': name})
-    #     self.render2('p/index.html', posts=[post])
-
-    # test slides
+    @tornado.gen.coroutine
+    @debug_wrapper
     def get(self, name):
-        self.render2('p/index.html')
+        log.info('GET PostPage for name {}'.format(name))
+
+        name = urllib.parse.quote(name).lower()
+        post = yield self.db.posts.find_one({'post_name': name})
+        if not post:
+            self.redirect_404()
+
+        post['post_type'] = post.get('post_type', 'text')
+        self.render2('p/index.html', posts=[post])
+
+    # # test slides
+    # def get(self, name):
+    #     self.render2('p/index.html')
 
 
 class MainPage(BaseHandler):
