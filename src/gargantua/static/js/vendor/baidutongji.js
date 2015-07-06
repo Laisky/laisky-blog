970fffa8473e73d48bf58c41c39f25232b58780b
Laisky <ppcelery@gmail.com>
2015-07-06 21:16:44 +0800

    #51 baidutongji

---
+++ src/gargantua/static/js/vendor/baidutongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?d863c60ff9993a7f26154e59a9c887e5";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
