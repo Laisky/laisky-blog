0729cbed9e840bf5d3c98efe4f7e10b69674b6ad
Laisky <ppcelery@gmail.com>
2015-07-23 10:32:24 +0800

    fix

--- src/gargantua/tasks/__init__.py
+++ src/gargantua/tasks/__init__.py
@@ -1,8 +1,13 @@
+from concurrent.futures import ThreadPoolExecutor
+
 import tornado.ioloop
 
 from .keyword import bind_task as bind_keyword_task
 
 
+executor = ThreadPoolExecutor(max_workers=2)
+
+
 def setup_tasks(ioloop=False):
     ioloop = ioloop or tornado.ioloop.IOLoop.instance()
-    bind_keyword_task(ioloop)
+    bind_keyword_task(ioloop, executor)
