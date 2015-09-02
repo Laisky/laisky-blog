15d6d27735627a471872a4b1300fd36d46196d4a
Laisky <ppcelery@gmail.com>
2015-09-02 09:28:26 +0800

    * #60 精简 markdown

--- src/gargantua/utils/markdown.py
+++ src/gargantua/utils/markdown.py
@@ -6,15 +6,11 @@ import re
 import markdown2
 
 
-H1_REG = re.compile(r'<h1>(.+)</h1>')
-H2_REG = re.compile(r'<h2>(.+)</h2>')
-H3_REG = re.compile(r'<h3>(.+)</h3>')
+TITLE_REG = re.compile(r'<(h[123])>(.+)</\1>')
 
 
 def render_md_to_html(content):
     html = markdown2.markdown(content, extras=['fenced-code-blocks', 'footnotes', 'tables'])
     _s = '<div class="highlight">{}</div>'.format(html)
-    _s = H1_REG.sub(r'<h1 id="\1">\1</h1>', _s)
-    _s = H2_REG.sub(r'<h2 id="\1">\1</h2>', _s)
-    _s = H3_REG.sub(r'<h3 id="\1">\1</h3>', _s)
+    _s = TITLE_REG.sub(r'<\1 id="\2">\2</\1>', _s)
     return _s
