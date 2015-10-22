4590749ba41b907c75abdc2be1002e377a1e22f5
Laisky <ppcelery@gmail.com>
2015-10-22 16:10:33 +0800

    * #62 用线上地址

--- tests/test_blog_page.py
+++ tests/test_blog_page.py
@@ -7,7 +7,7 @@ from tornado.testing import AsyncTestCase, gen_test
 class TestBlogPage(AsyncTestCase):
 
     def setUp(self):
-        self.prefix = 'http://localhost:27850/'
+        self.prefix = 'http://blog.laisky.com/'
         super().setUp()
 
     @gen_test
@@ -15,15 +15,15 @@ class TestBlogPage(AsyncTestCase):
         """测试博客首页的跳转和 404 页面
         """
         gid = 'UA-65521906-1'
-        expect_page_content = '<h1>Page Not Found</h1>'
 
         url = self.prefix
         httpclient = AsyncHTTPClient(self.io_loop)
         resp = yield httpclient.fetch(url)
 
         self.assertEqual(resp.code, 200)
-        self.assertEqual(urlparse(resp.effective_url).path, '/404.html')
+        self.assertEqual(urlparse(resp.effective_url).path, '/archives/')
         # check content
         content = resp.body.decode('utf-8')
         self.assertIn(gid, content)
-        self.assertIn(expect_page_content, content)
+        self.assertIn('class="post post-container">', content)
+        self.assertIn('<h3>Profile</h3>', content)
