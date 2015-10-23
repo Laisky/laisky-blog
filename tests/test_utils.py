b48a535b3420c6bd37780fa55364bc23a67d1859
Laisky <ppcelery@gmail.com>
2015-10-23 09:08:11 +0800

    * #62 增加 utils 的测试

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,6 +2,8 @@ import random
 from string import ascii_lowercase
 from unittest import TestCase
 
+import jwt
+
 from gargantua.utils import render_md_to_html, \
     generate_passwd, validate_passwd, generate_token, validate_token
 
@@ -27,9 +29,10 @@ class TestEncrypt(TestCase):
         self.assertTrue(validate_passwd(passwd, hashed))
         self.assertFalse(validate_passwd(wrong, hashed))
 
-
-    # def test_jwt(self):
-    #     passwd = ''.join([random.choice(ascii_lowercase) for _ in range(15)])
-    #     wrong = passwd[:-1] + '0'
-    #     secret = generate_token(passwd)
-    #     self.assertTrue(validate_token())
+    def test_jwt(self):
+        passwd = ''.join([random.choice(ascii_lowercase) for _ in range(15)])
+        wrong = passwd[:-1] + '0'
+        j = {'username': 'laisky'}
+        token = generate_token(j, passwd)
+        self.assertTrue(validate_token(token, passwd))
+        self.assertRaises(jwt.DecodeError, validate_token, token, wrong)
