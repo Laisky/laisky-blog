b45f22ce08e24d18031ec10eb57eecedfa99c6aa
Laisky <ppcelery@gmail.com>
2016-01-07 15:01:30 +0800

    + #73 pbr

--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
-from setuptools import setup, find_packages
-from pip.req import parse_requirements
-from pip.download import PipSession
+#!/usr/bin/env python
 
+from setuptools import setup
 
-requires = [str(i.req) for i in parse_requirements('requirements.txt',
-                                                   session=PipSession())
-            if i.req is not None]
-
-setup(name='gargantua',
-      version='2.0.5',
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      include_package_data=True,
-      install_requires=requires,
-      entry_points="""\
-      [console_scripts]
-      run_gargantua=gargantua.__main__:main
-      """)
-
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+)
