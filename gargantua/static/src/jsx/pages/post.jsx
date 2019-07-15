c52f99bd25f694c0f2e1527dafe774791c221a51
Laisky <ppcelery@gmail.com>
2019-07-15 15:31:12 +0800

    前端修复

--- gargantua/static/src/jsx/pages/post.jsx
+++ gargantua/static/src/jsx/pages/post.jsx
@@ -21,7 +21,7 @@ export class Post extends BaseComponent {
             post: null,
             hint: '载入中...'
         };
-    };
+    }
 
     componentDidMount() {
         $.getJSON({
@@ -30,7 +30,7 @@ export class Post extends BaseComponent {
             dataType: 'json'
         })
             .done((resp) => {
-                document.title = "laisky-blog: " + resp.result.post_title;
+                document.title = 'laisky-blog: ' + resp.result.post_title;
                 if(resp.result['post_type'] == 'slide') this.loadRevealJs();
                 resp.result.post_content = this.convertImg2Webp(resp.result.post_content);
                 $(document.body).animate({scrollTop: 0}, 200);
@@ -38,22 +38,24 @@ export class Post extends BaseComponent {
                     post: resp.result,
                     hint: null
                 });
+
+                $('#post').scrollspy({target: '#archive-menu'});
             })
             .fail(() => {
                 this.setState({hint: '读取数据失败，请刷新重试'});
             });
-    };
+    }
 
     // http://blog.qiniu.com/archives/5793
     // should only work for qiniu
     convertImg2Webp(content) {
-        if(navigator.browserInfo.name != 'Chrome') return content
+        if(navigator.browserInfo.name != 'Chrome') return content;
         return content.replace(
             // https://blog.laisky.com/qiniu/srceen_shot%202016-10-31%20at%2020.22.45.jpg
             /(\bhttps:\/\/blog\.laisky\.com\/qiniu\/[^\/]+\.(jpg|jpeg|gif|png))/ig,
             '$1?imageMogr2/format/webp'
-        )
-    };
+        );
+    }
 
     loadRevealJs() {
         $.getScript(window.revealLibUrl)
@@ -61,89 +63,62 @@ export class Post extends BaseComponent {
                 Reveal.initialize({
                     // Display controls in the bottom right corner
                     controls: true,
-
                     // Display a presentation progress bar
                     progress: true,
-
                     // Display the page number of the current slide
                     slideNumber: false,
-
                     // Push each slide change to the browser history
                     history: false,
-
                     // Enable keyboard shortcuts for navigation
                     keyboard: true,
-
                     // Enable the slide overview mode
                     overview: true,
-
                     // Vertical centering of slides
                     center: true,
-
                     // Enables touch navigation on devices with touch input
                     touch: true,
-
                     // Loop the presentation
                     loop: false,
-
                     // Change the presentation direction to be RTL
                     rtl: false,
-
                     // Randomizes the order of slides each time the presentation loads
                     shuffle: false,
-
                     // Turns fragments on and off globally
                     fragments: true,
-
                     // Flags if the presentation is running in an embedded mode,
                     // i.e. contained within a limited portion of the screen
                     embedded: false,
-
                     // Flags if we should show a help overlay when the questionmark
                     // key is pressed
                     help: true,
-
                     // Flags if speaker notes should be visible to all viewers
                     showNotes: false,
-
                     // Number of milliseconds between automatically proceeding to the
                     // next slide, disabled when set to 0, this value can be overwritten
                     // by using a data-autoslide attribute on your slides
                     autoSlide: 0,
-
                     // Stop auto-sliding after user input
                     autoSlideStoppable: true,
-
                     // Use this method for navigation when auto-sliding
                     autoSlideMethod: Reveal.navigateNext,
-
                     // Enable slide navigation via mouse wheel
                     mouseWheel: false,
-
                     // Hides the address bar on mobile devices
                     hideAddressBar: true,
-
                     // Opens links in an iframe preview overlay
                     previewLinks: false,
-
                     // Transition style
                     transition: 'default', // none/fade/slide/convex/concave/zoom
-
                     // Transition speed
                     transitionSpeed: 'default', // default/fast/slow
-
                     // Transition style for full page slide backgrounds
                     backgroundTransition: 'default', // none/fade/slide/convex/concave/zoom
-
                     // Number of slides away from the current that are visible
                     viewDistance: 3,
-
                     // Parallax background image
                     parallaxBackgroundImage: '', // e.g. "'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg'"
-
                     // Parallax background size
                     parallaxBackgroundSize: '', // CSS syntax, e.g. "2100px 900px"
-
                     // Number of pixels to move the parallax background per slide
                     // - Calculated automatically unless specified
                     // - Set to 0 to disable movement along an axis
@@ -151,7 +126,7 @@ export class Post extends BaseComponent {
                     parallaxBackgroundVertical: null,
                 });
             });
-    };
+    }
 
     render() {
         let hintEle,
@@ -184,7 +159,7 @@ export class Post extends BaseComponent {
         }
 
         return (
-            <div className="container-fluid post-body" id="post">
+            <div data-spy="scroll" data-target="#archive-menu" className="container-fluid post-body" id="post">
                 <div id="page-content" className={postMenu? 'col-sm-10 col-xs-12': 'container-fluid'}>
                     {hintEle}
                     {postContent}
@@ -192,8 +167,8 @@ export class Post extends BaseComponent {
                 </div>
                 {postMenu}
             </div>
-        )
-    };
+        );
+    }
 
 }
 
@@ -214,7 +189,7 @@ export class PostCategories extends BaseComponent {
     }
 
     async loadArticlesByCategoryId(cid) {
-        let url = `/api/v2/post/?truncate=0&limit=1000`,
+        let url = '/api/v2/post/?truncate=0&limit=1000',
             html = '';
         if(cid != undefined) url += `&category=${cid}`;
         $.getJSON(url)
@@ -224,7 +199,7 @@ export class PostCategories extends BaseComponent {
                 }
                 this.setState({categories: html});
             });
-    };
+    }
 
     render() {
         return (
@@ -235,5 +210,5 @@ export class PostCategories extends BaseComponent {
                 </div>
             </div>
         );
-    };
+    }
 }
