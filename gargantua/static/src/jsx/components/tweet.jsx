64ccd2c21a915a5bca627d7947c9f1d7ab5ffa9c
Laisky <ppcelery@gmail.com>
2021-03-30 05:26:00 +0000

    feat: tweet status

--- gargantua/static/src/jsx/components/tweet.jsx
+++ gargantua/static/src/jsx/components/tweet.jsx
@@ -20,7 +20,8 @@ export class TwitterPage extends BaseComponent {
 export class Tweet extends BaseComponent {
     componentDidMount() {
         this.setState({
-            text: '加载中…'
+            text: '加载中…',
+            images: [],
         });
 
         this.loadTweets();
@@ -48,13 +49,47 @@ export class Tweet extends BaseComponent {
             return;
         }
 
-        this.setState({ text: resp.TwitterStatues[0].text });
+        this.setState({
+            status_id: resp.TwitterStatues[0].id,
+            text: resp.TwitterStatues[0].text,
+            images: resp.TwitterStatues[0].images,
+        });
     }
 
     render() {
+        let images = [];
+        let defaultImg = '';
+
+        if (this.state.images) {
+            this.state.images.forEach(ele => {
+                images.push(
+                    <img src={ele} alt="" />
+                );
+            });
+
+            if (this.state.images.length != 0) {
+                defaultImg = this.state.images[0];
+            }
+        }
+
+
+        $('meta[name="twitter:card"]').remove();
+        $('meta[name="twitter:title"]').remove();
+        $('meta[name="twitter:description"]').remove();
+        $('meta[name="twitter:image"]').remove();
+        $('meta[name="twitter:site"]').remove();
+        $('head').append('<meta name="twitter:card" content="summary_large_image" />');
+        $('head').append(`<meta name="twitter:title" content="${this.state.status_id}" />`);
+        $('head').append(`<meta name="twitter:description" content="${this.state.text}" />`);
+        $('head').append(`<meta name="twitter:image" content="${defaultImg}" />`);
+        $('head').append(`<meta name="twitter:site" content="https://blog.laisky.com/twitter/status/${this.state.status_id}/" />`);
+
         return (
             <article>
                 {this.state.text}
+                <div className="images">
+                    {images}
+                </div>
             </article>
         );
     }
