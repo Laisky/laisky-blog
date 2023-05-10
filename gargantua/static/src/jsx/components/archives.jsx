86df300d0a2d487a15d706be1f370f81d25f4f06
Laisky.Cai <github@laisky.com>
2023-05-10 13:37:24 +0000

    refactor: Refactor archives component for clarity and organization
    - Remove comments related to `payment` and `comments` in `archives.jsx`
    - Comment out lines related to `comments` in `render` method in `archives.jsx`
    - Refactor code for improved readability and efficiency in `archives.jsx`
    - Update dependencies to latest versions for enhanced security and performance.

--- gargantua/static/src/jsx/components/archives.jsx
+++ gargantua/static/src/jsx/components/archives.jsx
@@ -137,13 +137,13 @@ class ArchiveExtract extends BaseComponent {
                 </div>,
                 // payment,
                 articleEditable,
-                <Link to={{ pathname: `/p/${archiveName}/#disqus_thread` }} data-disqus-identifier={archiveName} target="_blank">评论</Link>
+                // <Link to={{ pathname: `/p/${archiveName}/#disqus_thread` }} data-disqus-identifier={archiveName} target="_blank">评论</Link>
             ];
         }
 
         return [
             articleEditable,
-            <Link to={{ pathname: `/p/${archiveName}/#disqus_thread` }} data-disqus-identifier={archiveName} target="_blank">评论</Link>
+            // <Link to={{ pathname: `/p/${archiveName}/#disqus_thread` }} data-disqus-identifier={archiveName} target="_blank">评论</Link>
         ];
     }
 
