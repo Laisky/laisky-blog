a96db8f9d4bd09da751bb8e8380c8c3172d444de
Laisky.Cai <github@laisky.com>
2024-01-21 14:36:10 +0000

    feat: Add language dropdown and support for Chinese translation
    - Add a dropdown to change the blog language.
    - Add static translations for Chinese and English.
    - Improve the language dropdown styling.
    - Fix the language display.

--- gargantua/static/src/jsx/pages/app.jsx
+++ gargantua/static/src/jsx/pages/app.jsx
@@ -34,8 +34,41 @@ class App extends BaseComponent {
         };
     }
 
+    componentDidMount() {
+        document
+            .querySelectorAll('.navbar-fixed-top .dropdown.language .dropdown-menu li')
+            .forEach((ele) => {
+                ele.addEventListener('click', (evt) => {
+                    let userLang = window.getUserLanguage();
+
+                    let target = evt.target;
+                    if (target.tagName != 'LI') {
+                        target = target.parentElement;
+                    }
+
+                    if (userLang == target.dataset.lang) return;
+
+                    let url = new URL(window.location.href);
+                    url.searchParams.set('lang', target.dataset.langSimple);
+                    url.searchParams.set('force', 1);
+
+                    // reload page
+                    window.location.href = url.toString();
+                });
+            });
+    }
+
     render() {
         let googleSearch = '<gcse:search className="google-search" gname="post_search" enableAutoComplete="true"></gcse:search>';
+
+        let userLang = window.getUserLanguage();
+        let dropdownBtn = <li className="dropdown language">
+            <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{userLang} <span className="caret"></span></a>
+            <ul className="dropdown-menu">
+                <li className={userLang == 'zh_CN' ? 'active' : ''} data-lang-simple='zh' data-lang='zh_CN'><a href="#">zh_CN</a></li>
+                <li className={userLang == 'en_US' ? 'active' : ''} data-lang-simple='en' data-lang='en_US'><a href="#">en_US</a></li>
+            </ul>
+        </li>;
 
         return (
             <div className="container-fluid">
@@ -62,9 +95,7 @@ class App extends BaseComponent {
                                 <li><a href="https://status.laisky.com" target="_blank">Status</a></li>
                             </ul>
                             <ul className="nav navbar-nav navbar-right">
-                                <li>
-                                    <a>{window.getUserLanguage()}</a>
-                                </li>
+                                {dropdownBtn}
                                 <li>
                                     <Link to="https://s3.laisky.com/public/rss.xml" target="_blank"><img src="https://s3.laisky.com/uploads/images/rss.png" className="rss"></img></Link>
                                 </li>
